5dfdd8e8de6d32f99bb71cb749af80034a80f072
Billy <koswarabilly@gmail.com>
2022-02-19 16:34:35 +0700

    feat: update styling and add library

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import { Input, InputGroup, InputRightElement, Button, HStack, Skeleton } from '@chakra-ui/react';
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  HStack,
+  Skeleton,
+  VStack,
+  Container,
+} from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 
 function Header() {
   return (
-    <div className="fixed w-screen bg-slate-500">
-      <div className="p-4">
+    <VStack
+      backgroundColor="white"
+      width="100vw"
+      position="fixed"
+      py="1rem"
+      alignItems="flex-start"
+    >
+      <Container px="1rem" maxWidth="auto">
         <Skeleton isLoaded>
-          <InputGroup>
+          <InputGroup width="100%">
             <Input placeholder="Search by symbol or name" />
             <InputRightElement>
               <SearchIcon />
             </InputRightElement>
           </InputGroup>
         </Skeleton>
-      </div>
-      <Skeleton isLoaded>
-        <HStack className="overflow-auto px-4 pb-4">
-          <Button className="block">Tag 11111111111111111</Button>
-          <Button>Tag 2</Button>
-          <Button>Tag 3</Button>
-          <Button>Tag 1</Button>
-          <Button>Tag 2</Button>
-          <Button>Tag 3</Button>
-          <Button>Tag 1</Button>
-          <Button>Tag 2</Button>
-          <Button>Tag 3</Button>
-        </HStack>
-      </Skeleton>
-    </div>
+      </Container>
+      <HStack overflowX="auto" px="1rem" pt="0.5rem" width="100vw" className="invisible-scrollbar">
+        <Skeleton isLoaded>
+          <Button minWidth="fit-content">All</Button>
+        </Skeleton>
+      </HStack>
+    </VStack>
   );
 }
 
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Box, Text, HStack, VStack, Image } from '@chakra-ui/react';
 
 function List() {
   return (
-    <div className="pt-20">
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-      <p className="text-9xl">This is List</p>
-    </div>
+    <VStack pt="8rem" px="1rem" pb="1rem">
+      <Box p="1rem" shadow="sm" borderWidth="1px" borderRadius="md" width="100%">
+        <Image src="https://s3-ap-southeast-1.amazonaws.com/static.pintu.co.id/assets/images/logo/circle_BTC.svg" />
+        <HStack>
+          <Text>BTC</Text>
+          <Text>Bitcoin</Text>
+        </HStack>
+        <VStack>
+          <HStack justifyContent="space-between" width="100%">
+            <Text>Last price</Text>
+            <Text>Last price</Text>
+          </HStack>
+          <HStack justifyContent="space-between" width="100%">
+            <Text>Last price</Text>
+            <Text>Last price</Text>
+          </HStack>
+          <HStack justifyContent="space-between" width="100%">
+            <Text>Last price</Text>
+            <Text>Last price</Text>
+          </HStack>
+        </VStack>
+      </Box>
+    </VStack>
   );
 }
 
